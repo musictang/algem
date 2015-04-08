616e4c53fb4e9a238580d2361284b307ea333f3e
Alexandre <alexd6631@gmail.com>
2015-04-08 10:36:24 +0200

    ISO-8859-1 used for CSV script output

--- src/net/algem/script/execution/ScriptExportServiceImpl.java
+++ src/net/algem/script/execution/ScriptExportServiceImpl.java
@@ -5,14 +5,16 @@ import org.supercsv.io.CsvListWriter;
 import org.supercsv.prefs.CsvPreference;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.OutputStreamWriter;
 import java.util.Collections;
 import java.util.List;
 
 public class ScriptExportServiceImpl implements ScriptExportService {
     @Override
     public void exportScriptResult(ScriptResult scriptResult, File outFile) throws Exception {
-        try (CsvListWriter csvListWriter = new CsvListWriter(new FileWriter(outFile), CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE)) {
+        try (CsvListWriter csvListWriter = new CsvListWriter(new OutputStreamWriter(new FileOutputStream(outFile), "iso-8859-1"), CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE)) {
             String[] header = new String[scriptResult.getHeader().size()];
             header = scriptResult.getHeader().toArray(header);
             csvListWriter.writeHeader(header);
