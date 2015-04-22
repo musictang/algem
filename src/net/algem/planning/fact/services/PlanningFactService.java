11255dcce72cf968d9445eed96a2bb68d7ce2c04
Alexandre <alexd6631@gmail.com>
2015-04-22 18:09:55 +0200

    Planning fact feature : - Replanification of planning WIP

--- src/net/algem/planning/fact/services/PlanningFactService.java
+++ src/net/algem/planning/fact/services/PlanningFactService.java
@@ -1,10 +1,18 @@
 package net.algem.planning.fact.services;
 
-import net.algem.planning.*;
+import net.algem.planning.DateFr;
+import net.algem.planning.Hour;
+import net.algem.planning.PlanningService;
+import net.algem.planning.Schedule;
 import net.algem.room.Room;
 import net.algem.util.DataCache;
 import net.algem.util.DataConnection;
+import net.algem.util.StringUtils;
 import net.algem.util.model.Model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Service class, to perform auditable operation on planning (schedule) instances.
@@ -26,7 +34,7 @@ public class PlanningFactService {
     /**
      * Move a schedule in a catch up room, and save an ABSENCE for the related prof.
      *
-     * @param schedule the schedule to
+     * @param schedule the schedule to move in catchup room
      * @param room the catchup room
      * @param commentaire user comment
      * @throws Exception
@@ -50,8 +58,8 @@ public class PlanningFactService {
 
     /**
      * Delete a schedule, and save a ACTIVITE_BAISSE fact for this event.
-     * @param schedule
-     * @param commentaire
+     * @param schedule the schedule to delete
+     * @param commentaire user comment
      * @throws Exception
      */
     public void scheduleDeletedLowActivity(final Schedule schedule, final String commentaire) throws Exception {
@@ -64,5 +72,87 @@ public class PlanningFactService {
                 return null;
             }
         });
+    }
+
+    public boolean isInAbsence(Schedule schedule) throws SQLException {
+        Room room = (Room) DataCache.findId(schedule.getIdRoom(), Model.Room);
+        return room != null && room.isCatchingUp();
+    }
+
+    /**
+     * Replanify a schedule, according to the replanification command.
+     * Absence, catchup and replacement facts, will be automatically created from the command,
+     * according to the <a href="https://trello.com/c/3WmPBMdy">specification</a>
+     *
+     * @param cmd a command that describes which part
+     * @param comment user
+     * @throws Exception
+     */
+    public void replanify(final ReplanifyCommand cmd, final String comment) throws Exception {
+        dc.withTransaction(new DataConnection.SQLRunnable<Void>() {
+            @Override
+            public Void run(DataConnection conn) throws Exception {
+                checkRoom(cmd);
+                List<PlanningFact> facts = createReplanifyFacts(cmd, comment);
+                for (PlanningFact fact : facts) {
+                    planningFactDAO.insert(fact);
+                }
+                executeReplanifyUpdate(cmd);
+                return null;
+            }
+        });
+    }
+
+    private List<PlanningFact> createReplanifyFacts(ReplanifyCommand cmd, String comment) throws SQLException {
+        List<PlanningFact> facts = new ArrayList<>();
+        Schedule schedule = cmd.getSchedule();
+        boolean scheduleInAbsence = isInAbsence(schedule);
+
+        if (cmd.getProfId().isPresent()) {
+            if (!scheduleInAbsence) {
+                facts.add(factCreator.createFactForPlanning(schedule, PlanningFact.Type.ABSENCE, comment));
+            }
+            facts.add(factCreator.createFactForPlanning(schedule, cmd.getProfId().get(), PlanningFact.Type.REMPLACEMENT, comment));
+        }
+        else if (cmd.getDate().isPresent()) {
+            if (!scheduleInAbsence) {
+                facts.add(factCreator.createFactForPlanning(schedule, PlanningFact.Type.ABSENCE, comment));
+            }
+            facts.add(factCreator.createFactForPlanning(schedule, PlanningFact.Type.RATTRAPAGE, comment));
+        }
+        return facts;
+    }
+
+
+    private void checkRoom(ReplanifyCommand cmd) throws SQLException {
+        for (int salle : cmd.getRoomId()) {
+            Room room = (Room) DataCache.findId(cmd.getSchedule().getIdRoom(), Model.Room);
+            if (room != null && room.isCatchingUp()) {
+                throw new IllegalArgumentException("Cannot replanify to a catching up room");
+            }
+        }
+    }
+
+    private void executeReplanifyUpdate(ReplanifyCommand cmd) throws SQLException {
+        List<String> updates = new ArrayList<>();
+        for (int prof : cmd.getProfId()) {
+            updates.add("prof = " + prof);
+        }
+        for (DateFr dateFr : cmd.getDate()) {
+            updates.add("date = " + dateFr);
+        }
+        for (Hour hour : cmd.getStartHour()) {
+            int durationMinutes = cmd.getSchedule().getStart().getLength(cmd.getSchedule().getEnd());
+            updates.add("debut = " + hour);
+            Hour endHour = new Hour(hour);
+            endHour.incMinute(durationMinutes);
+            updates.add("fin = " + endHour);
+        }
+        for (int salle : cmd.getRoomId()) {
+            updates.add("lieux = " + salle);
+        }
+
+        String setPart = StringUtils.join(updates, ", ");
+        dc.executeQuery("UPDATE planning SET " + setPart + " WHERE id = " + cmd.getSchedule().getId());
     }
 }
---
+++ src/net/algem/util/Option.java
@@ -0,0 +1,87 @@
+package net.algem.util;
+
+import net.algem.planning.fact.services.ReplanifyCommand;
+
+import java.util.Iterator;
+
+/**
+ * Lightweight Option type, that behaves like a zero or one element list
+ */
+public class Option<T> implements Iterable<T> {
+    @Override
+    public Iterator<T> iterator() {
+        if (data != null) {
+            return new Iterator<T>() {
+                boolean hasNext;
+                @Override
+                public boolean hasNext() {
+                    return hasNext;
+                }
+
+                @Override
+                public T next() {
+                    return null;
+                }
+
+                @Override
+                public void remove() {
+
+                }
+            };
+        } else {
+            return new Iterator<T>() {
+                @Override
+                public boolean hasNext() {
+                    return false;
+                }
+
+                @Override
+                public T next() {
+                    return null;
+                }
+
+                @Override
+                public void remove() {
+
+                }
+            };
+        }
+    }
+
+    public boolean isPresent() {
+        return data != null;
+    }
+
+    public T get() {
+        return data;
+    }
+
+    private T data;
+
+    private Option(T data) {
+        this.data = data;
+    }
+
+    public static <T> Option<T> of(T data) {
+        return new Option<>(data);
+    }
+    public static <T> Option<T> none() {
+        return new Option<>(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Option<?> option = (Option<?>) o;
+
+        return !(data != null ? !data.equals(option.data) : option.data != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return data != null ? data.hashCode() : 0;
+    }
+}
