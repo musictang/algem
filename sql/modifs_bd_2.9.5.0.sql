d5357a40089965b41a886c3b8bcaa31479da3ea8
Jean-Marc <jmg@musiques-tangentes.asso.fr>
2016-01-19 19:53:20 +0100

    algem 2.9.5.0-alpha00

---
+++ sql/modifs_bd_2.9.5.0.sql
@@ -0,0 +1,14 @@
+-- 2.9.5.0
+CREATE TABLE jeton_login(
+    idper PRIMARY KEY,
+    jeton varchar(64),
+    creadate timestamp
+);
+
+COMMENT ON TABLE jeton_login IS 'Jeton temporaire pour la modification du mot de passe';
+COMMENT ON COLUMN jeton_login.idper IS 'Identifiant de la personne';
+COMMENT ON COLUMN jeton_login.jeton IS 'UUID';
+COMMENT ON COLUMN jeton_login.creadate IS 'Date de création du jeton';
+-- -- See comments with psql : \d+ jeton_login 
+
+ALTER TABLE jeton_login OWNER TO nobody;
