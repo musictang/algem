050d10f2e22bab0c2ebc2aaeee8a4b1194908812
Jean-Marc <jmg@musiques-tangentes.asso.fr>
2016-02-02 19:54:51 +0100

    algem 2.9.5.0-alpha03

--- sql/modifs_bd_2.9.5.0.sql
+++ sql/modifs_bd_2.9.5.0.sql
@@ -4,7 +4,6 @@ CREATE TABLE jeton_login(
     jeton varchar(64),
     creadate timestamp
 );
-
 COMMENT ON TABLE jeton_login IS 'Jeton temporaire pour la modification du mot de passe';
 COMMENT ON COLUMN jeton_login.idper IS 'Identifiant de la personne';
 COMMENT ON COLUMN jeton_login.jeton IS 'UUID';
@@ -13,5 +12,24 @@ COMMENT ON COLUMN jeton_login.creadate IS 'Date de création du jeton';
 
 ALTER TABLE jeton_login OWNER TO nobody;
 
-INSERT INTO config VALUES ('Reservation.delai',24);
+CREATE TABLE reservation(
+    id serial PRIMARY KEY,
+    idper integer,
+    typeres integer,
+    dateres timestamp DEFAULT 'now',
+    pass boolean DEFAULT FALSE
+);
+
+COMMENT ON TABLE reservation IS 'Stockage temporaire des informations de réservation';
+COMMENT ON COLUMN reservation.idper IS 'Identifiant de la personne ayant réservé';  
+COMMENT ON COLUMN reservation.typeres IS 'Type de réservation : individuelle, groupe'; 
+COMMENT ON COLUMN reservation.dateres IS 'Date de soumission de la réservation';    
+COMMENT ON COLUMN reservation.pass IS 'Sélection carte d''abonnement';   
+
+ALTER TABLE reservation OWNER TO nobody;
+-- delai minimum de réservation en heures
+INSERT INTO config VALUES ('Reservation.delai.min',24);
+-- delai d'annulation en heures
 INSERT INTO config VALUES ('Annulation.delai',72);
+-- delai max de réservation en jours
+INSERT INTO config VALUES ('Reservation.delai.max',15);
