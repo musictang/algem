1463c587e5132e1d55d9d44b83e5196b9915c3e5
jm <jmao@free.fr>
2016-02-08 10:36:08 +0100

    algem 2.9.5.0-alpha05

--- sql/modifs_bd_2.9.5.0.sql
+++ sql/modifs_bd_2.9.5.0.sql
@@ -17,14 +17,18 @@ CREATE TABLE reservation(
     idper integer,
     idaction integer,
     dateres timestamp DEFAULT 'now',
-    pass boolean DEFAULT FALSE
+    pass boolean DEFAULT FALSE,
+    statut smallint DEFAULT 0 -- 0 (en attente de confirmation),1 (confirmée)
 );
+
+-- TEMP,CONF,ANNL, 0,1,-1
 
 COMMENT ON TABLE reservation IS 'Stockage temporaire des informations de réservation';
 COMMENT ON COLUMN reservation.idper IS 'Identifiant de la personne ayant réservé';  
 COMMENT ON COLUMN reservation.idaction IS 'Action sur le planning'; 
 COMMENT ON COLUMN reservation.dateres IS 'Date de soumission de la réservation';    
-COMMENT ON COLUMN reservation.pass IS 'Sélection carte d''abonnement';   
+COMMENT ON COLUMN reservation.pass IS 'Sélection carte d''abonnement';
+COMMENT ON COLUMN reservation.statut IS '0 = en attente de confirmation, 1 = confirmée';
 
 ALTER TABLE reservation OWNER TO nobody;
 -- delai minimum de réservation en heures
