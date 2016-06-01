c7336747674e8c7a019733e42509bc940d47c22a
Jean-Marc <jmg@musiques-tangentes.asso.fr>
2016-06-01 10:51:27 +0200

    2.9.7.2 final

--- sql/modifs_bd_2.9.7.sql
+++ sql/modifs_bd_2.9.7.sql
@@ -1,2 +1,8 @@
 -- 2.9.7
+-- fix technicien
+CREATE TABLE technicien(
+    idper integer PRIMARY KEY REFERENCES personne(id) ON DELETE CASCADE,
+    specialite integer DEFAULT 0 NOT NULL REFERENCES categorie_studio(id) ON DELETE SET DEFAULT
+);
 ALTER TABLE technicien OWNER TO nobody;
+
--- src/net/algem/Algem.java
+++ src/net/algem/Algem.java
@@ -1,5 +1,5 @@
 /*
- * @(#)Algem.java	2.9.7.1 25/05/16
+ * @(#)Algem.java	2.9.7.2 30/05/16
  *
  * Copyright (c) 1999-2016 Musiques Tangentes. All Rights Reserved.
  *
@@ -57,12 +57,12 @@ import org.apache.commons.codec.binary.Base64;
  *
  * @author <a href="mailto:eric@musiques-tangentes.asso.fr">Eric</a>
  * @author <a href="mailto:jmg@musiques-tangentes.asso.fr">Jean-Marc Gobat</a>
- * @version 2.9.7.1
+ * @version 2.9.7.2
  */
 public class Algem
 {
 
-  public static final String APP_VERSION = "2.9.7.1";
+  public static final String APP_VERSION = "2.9.7.2";
   public static List<LookAndFeelInfo> ALTERNATIVE_LAF = new ArrayList<>();
   private static final int DEF_WIDTH = 1080;// (850,650) => ancienne taille
   private static final int DEF_HEIGHT = 780;
