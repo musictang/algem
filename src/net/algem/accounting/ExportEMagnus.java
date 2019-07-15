965bce186f0e02aead5e7fce8e632c6b2ec7094d
eric <eric@devmad.fr>
2019-07-15 10:05:05 +0200

    renomme label "Pseudo" en "Alias EMagnus"

--- src/net/algem/accounting/ExportEMagnus.java
+++ src/net/algem/accounting/ExportEMagnus.java
@@ -19,12 +19,8 @@
  */
 package net.algem.accounting;
 
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.NumberFormat;
@@ -32,16 +28,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Vector;
-import net.algem.billing.VatIO;
 import net.algem.contact.Person;
 import net.algem.contact.member.Member;
 import net.algem.planning.DateFr;
 import net.algem.util.DataCache;
 import net.algem.util.DataConnection;
-import net.algem.util.MessageUtil;
-import net.algem.util.TextUtil;
 import net.algem.util.model.Model;
-import net.algem.util.ui.MessagePopup;
 
 /**
  *
@@ -50,120 +42,115 @@ import net.algem.util.ui.MessagePopup;
  * @since 2.17.0 01/07/2019
  */
 public class ExportEMagnus
-        extends CommonAccountExportService
-{
+        extends CommonAccountExportService {
 
-  private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-  private final NumberFormat nf = NumberFormat.getInstance(Locale.FRENCH);
-  private DataCache dataCache;
+    private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    private final NumberFormat nf = NumberFormat.getInstance(Locale.FRENCH);
+    private final DataCache dataCache;
 
-  public ExportEMagnus(DataConnection dc) {
-    dbx = dc;
-    
-    dataCache = DataCache.getInitializedInstance();
-    
-    journalService = new JournalAccountService(dc);
-    nf.setGroupingUsed(false);
-    nf.setMinimumFractionDigits(4);
-    nf.setMaximumFractionDigits(4);
-  }
+    public ExportEMagnus(DataConnection dc) {
+        dbx = dc;
 
-  @Override
-  public String getFileExtension() {
-    return ".txt";
-  }
+        dataCache = DataCache.getInitializedInstance();
 
-  @Override
-  public void export(String path, Vector<OrderLine> lines, String codeJournal, Account documentAccount) throws IOException {
-    int totalDebit = 0;
-    int totalCredit = 0;
-    String number = (documentAccount == null) ? "" : documentAccount.getNumber();
-    OrderLine e = null;
-    
-    //try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(path), "windows-1252"), true)) {
-//    try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
-    try (PrintWriter out = new PrintWriter(path,"cp1252")) {
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0, n = lines.size(); i < n; i++) {
-        e = lines.elementAt(i);
-      
-        Person payer=new Person();
-        Person student=new Person();
-        Member member=new Member(0);
-        try {
-            payer = (Person)dataCache.findId(e.getPayer(), Model.Person);
-            student = (Person)dataCache.findId(e.getMember(), Model.Person);
-            member = (Member)dataCache.findId(e.getMember(), Model.Member);
-        } catch (SQLException ex) {}
-
-        //colonne A
-        sb.append(i+1);
-        sb.append(";T;O;CCMDL;MDL;");
-        //colonne F
-        sb.append(e.getDate().getYear()).append(";");
-        sb.append(';');
-        //colonne H
-        sb.append('"').append(payer.getFirstnameName()).append('"').append(";");
-        //sb.append('"').append(payeur.getNickName()).append('"').append(";");
-        
-        sb.append(";;;;;;;;;");
-        //colonne R
-        sb.append('"').append("Facturation école ressouce semestre année ").append(dataCache.getSchoolYearLabel()).append('"').append(';');
-        sb.append(';');
-        //colonne T
-        String label = e.getLabel() == null ? "" : e.getLabel().replaceAll("\"", "");
-        sb.append('"').append(student.getFirstnameName()).append(' ').append(label).append('"').append(';');
-        
-        sb.append(";;;R;");
-        //colonne Y
-        sb.append(new DateFr(new Date())).append(';');
-        
-        //colonne Z/AB/AC à mettre en config
-        sb.append("7062;;BP3001;33;");
-        
-        //colonne AD Mnt HT
-        sb.append(nf.format(e.getAmount() / 100.0)).append(';');
-        
-        sb.append("0;0;0;0;9;;-1;");
-        
-        //colonne AL
-        sb.append("0;;;;;;;;;;;;;;;;");
-        
-        //colonne BB à mettre en config
-        sb.append("94;");
-        
-        sb.append(";;;;;;;;;;;;;");
-        
-        //colonne BP
-        sb.append('"').append("le tribunal administratif").append('"').append(';');
-        
-        sb.append(";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
-        
-        //colonne DC
-        if (member != null && member.getBirth() != null) {
-            sb.append(member.getBirth()).append(';');
-        } else {
-            sb.append(';');
-        }
-        
-        sb.append(";;0;0;;;;;;;;;;;;;;0;");
-                
-        // fin = colonne DU
-
-        sb.append("\r\n");
-        out.print(sb.toString());
-//        out.println(sb.toString());
-        sb.delete(0, sb.length());
-      }
+        journalService = new JournalAccountService(dc);
+        nf.setGroupingUsed(false);
+        nf.setMinimumFractionDigits(4);
+        nf.setMaximumFractionDigits(4);
     }
 
-  }
+    @Override
+    public String getFileExtension() {
+        return ".txt";
+    }
 
-  @Override
-  public int tiersExport(String path, Vector<OrderLine> lines) throws IOException, SQLException {
-      System.out.println("ExportEMagnus.tiersExport");
-    int errors = 0;
-    return errors;
-  }
+    @Override
+    public void export(String path, Vector<OrderLine> lines, String codeJournal, Account documentAccount) throws IOException {
+        OrderLine e = null;
+
+        //try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(path), "windows-1252"), true)) {
+        try (PrintWriter out = new PrintWriter(path, "cp1252")) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0, n = lines.size(); i < n; i++) {
+                e = lines.elementAt(i);
+
+                Person payer = new Person();
+                Person student = new Person();
+                Member member = new Member(0);
+                try {
+                    payer = (Person) dataCache.findId(e.getPayer(), Model.Person);
+                    student = e.getPayer() == e.getMember() ? payer : (Person) dataCache.findId(e.getMember(), Model.Person);
+                    member = (Member) dataCache.findId(e.getMember(), Model.Member);
+                } catch (SQLException ex) {
+                }
+
+                //colonne A
+                sb.append(i + 1);
+                sb.append(";T;O;CCMDL;MDL;");
+                //colonne F
+                sb.append(e.getDate().getYear()).append(";");
+                sb.append(';');
+                //colonne H
+                sb.append('"').append(payer.getNickName()).append('"').append(";");
+
+                sb.append(";;;;;;;;;");
+                //colonne R
+                sb.append('"').append("Facturation école ressouce semestre année ").append(dataCache.getSchoolYearLabel()).append('"').append(';');
+                sb.append(';');
+                //colonne T
+                String label = e.getLabel() == null ? "" : e.getLabel().replaceAll("\"", "");
+                sb.append('"').append(student.getFirstnameName()).append(' ').append(label).append('"').append(';');
+
+                sb.append(";;;R;");
+                //colonne Y
+                sb.append(new DateFr(new Date())).append(';');
+
+                //colonne Z/AB/AC à mettre en config
+                sb.append("7062;;BP3001;33;");
+
+                //colonne AD Mnt HT
+                sb.append(nf.format(e.getAmount() / 100.0)).append(';');
+
+                sb.append("0;0;0;0;9;;-1;");
+
+                //colonne AL
+                sb.append("0;;;;;;;;;;;;;;;;");
+
+                //colonne BB à mettre en config
+                sb.append("94;");
+
+                sb.append(";;;;;;;;;;;;;");
+
+                //colonne BP
+                sb.append('"').append("le tribunal administratif").append('"').append(';');
+
+                sb.append(";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
+
+                //colonne DC
+                if (member != null && member.getBirth() != null) {
+                    sb.append(member.getBirth()).append(';');
+                } else {
+                    sb.append(';');
+                }
+
+                sb.append(";;0;0;;;;;;;;;;;;;;0;");
+
+                // fin = colonne DU
+                sb.append("\r\n");
+
+                out.print(sb.toString());
+
+                sb.delete(0, sb.length());
+            }
+        }
+
+    }
+
+    @Override
+    public int tiersExport(String path, Vector<OrderLine> lines) throws IOException, SQLException {
+        System.out.println("ExportEMagnus.tiersExport");
+        int errors = 0;
+        return errors;
+    }
 
 }
